d35c09ace758b4d242f291c1d4d6b9d75853c939
#云淡然 <cloudcome@163.com>
2016-05-20 18:04:49 +0800

    blear.ui.alert

---
+++ readme.md
@@ -0,0 +1,15 @@
+# blear.ui.alert
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.ui.alert/master.svg?maxAge=2592000&style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.ui.alert
+
+[npm-img]: https://img.shields.io/npm/v/blear.ui.alert.svg?maxAge=2592000&style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.ui.alert
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.ui.alert/master.svg?maxAge=2592000&style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.ui.alert?branch=master
+
---
+++ src/index.js
@@ -0,0 +1,55 @@
+/**
+ * modern-alert
+ * @author ydr.me
+ * @create 2016-04-22 20:03
+ */
+
+
+
+'use strict';
+
+var Dialog =   require('blear.ui.dialog');
+var object =   require('blear.utils.object');
+var Template = require('blear.classes.template');
+var template = require('./template.html', 'html');
+
+
+var tpl = new Template(template);
+var defaults = {
+    width: 300,
+    sure: {
+        text: '好',
+        type: 'primary'
+    },
+    title: '提醒',
+    message: '',
+    addClass: ''
+};
+
+var Alert = Dialog.extend({
+    className: 'Alert',
+    constructor: function (options) {
+        var the = this;
+
+        options = object.assign(true, {}, defaults, options);
+        the.Super({
+            buttons: [options.sure],
+            closeable: false,
+            headable: true,
+            title: options.title,
+            width: options.width,
+            addClass: options.addClass,
+            template: tpl.render({options: options})
+        });
+
+        // init event
+        the.on('action', function () {
+            the.emit('sure');
+            the.close();
+        });
+    }
+});
+
+require('./style.css', 'css|style');
+Alert.defaults = defaults;
+module.exports = Alert;
